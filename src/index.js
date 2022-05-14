ebdc8ad58cc4331251fe403355a9d5a2aafc111a
x22tri <x22tri@gmail.com>
2022-05-14 11:33:51 +0200

    Fix index after update to React 18

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./components/App"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './components/App'
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+)
