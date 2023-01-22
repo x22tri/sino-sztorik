46012da42eb9c63f252048445a10e8d9c8910329
x22tri <x22tri@gmail.com>
2023-01-22 21:29:42 +0100

    Add fade effect to PreviewRow scroll

--- src/new-components/lesson-select/preview-row/PreviewRow.tsx
+++ src/new-components/lesson-select/preview-row/PreviewRow.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   List,
   ListItem,
   ListItemButton,
@@ -8,8 +9,9 @@ import {
 } from '@mui/material'
 import { AssembledLesson } from '../../shared/interfaces'
 import { TierStatusCircle } from '../tier-status-circle/TierStatusCircle'
-import { Dispatch, SetStateAction, useState } from 'react'
+import { Dispatch, ReactNode, SetStateAction, useState } from 'react'
 import { useSmallScreen } from '../../shared/utility-functions'
+import { Wrap } from '../../shared/utility-components'
 
 export function PreviewRow({
   lessons,
@@ -34,63 +36,126 @@ export function PreviewRow({
     setCanScrollDown(scrollHeight - scrollTop !== clientHeight)
   }
 
+  const minWidth = '360px'
+  const height = '480px'
+
   return (
-    <List
-      disablePadding
-      onScroll={({ target }) => handleScroll(target as HTMLUListElement)}
+    <Box
+      marginTop={1}
+      position='relative'
       sx={{
-        mt: 1,
-        ml: 1,
-        borderTop: canScrollUp ? `2px solid ${palette.grey[300]}` : null,
-        borderBottom: canScrollDown ? `2px solid ${palette.grey[300]}` : null,
-        minWidth: '360px',
-        height: isSmallScreen ? undefined : '480px',
-        overflow: isSmallScreen ? 'none' : 'scroll',
+        ':after': {
+          background: `linear-gradient(180deg, 
+${canScrollUp ? `${palette.background.default} 0%, rgba(0,0,0,0) 10%` : ''} 
+${canScrollUp && canScrollDown ? ',' : ''}
+${canScrollDown ? `rgba(0,0,0,0) 90%, ${palette.background.default} 100%` : ''}
+          )`,
+          bottom: 0,
+          content: '""',
+          height: '100%',
+          pointerEvents: 'none',
+          position: 'absolute',
+          width: '100%',
+        },
       }}
     >
-      {lessons.map(({ lessonNumber, tierStatuses }) => (
-        <ListItem
-          disablePadding
-          disableGutters
-          key={lessonNumber}
-          sx={{ mt: lessonNumber === 1 ? 0 : 1 }}
-          // sx={{ my: 1 }}
-        >
-          <ListItemButton
+      <List
+        disablePadding
+        onScroll={({ target }) => handleScroll(target as HTMLUListElement)}
+        sx={{
+          ml: 1,
+          pr: 1,
+          borderTop: canScrollUp ? `2px solid ${palette.grey[300]}` : null,
+          borderBottom: canScrollDown ? `2px solid ${palette.grey[300]}` : null,
+          minWidth,
+          height: isSmallScreen ? undefined : height,
+          overflow: isSmallScreen ? 'none' : 'scroll',
+        }}
+      >
+        {lessons.map(({ lessonNumber, tierStatuses }) => (
+          <ListItem
+            disablePadding
             disableGutters
-            onClick={() => setSelectedLessonNumber(lessonNumber)}
-            sx={{
-              backgroundColor: 'background.paper',
-              border: `1px solid ${palette.grey[200]}`,
-              borderRadius: 2,
-              // mx: 1,
-              padding: 0,
-              '.MuiListItemText-multiline': {
-                display: 'flex',
-                flexDirection: 'column-reverse',
-              },
-            }}
+            key={lessonNumber}
+            sx={{ mt: lessonNumber === 1 ? 0 : 1 }}
           >
-            <ListItemIcon>
-              <TierStatusCircle {...{ lessonNumber, tierStatuses }} />
-            </ListItemIcon>
-            <ListItemText
-              primary='Lecke címe'
-              secondary='11 karakter'
+            <ListItemButton
+              disableGutters
+              onClick={() => setSelectedLessonNumber(lessonNumber)}
               sx={{
-                '.MuiListItemText-primary': {
-                  ...typography.titleSubtitle.subtitle,
-                  color: palette.text.secondary,
-                },
-                '.MuiListItemText-secondary': {
-                  ...typography.titleSubtitle.title,
-                  color: palette.text.disabled,
+                backgroundColor: 'background.paper',
+                border: `1px solid ${palette.grey[200]}`,
+                borderRadius: 2,
+                // mx: 1,
+                padding: 0,
+                '.MuiListItemText-multiline': {
+                  display: 'flex',
+                  flexDirection: 'column-reverse',
                 },
               }}
-            />
-          </ListItemButton>
-        </ListItem>
-      ))}
-    </List>
+            >
+              <ListItemIcon>
+                <TierStatusCircle {...{ lessonNumber, tierStatuses }} />
+              </ListItemIcon>
+              <ListItemText
+                primary='Lecke címe'
+                secondary='11 karakter'
+                sx={{
+                  '.MuiListItemText-primary': {
+                    ...typography.titleSubtitle.subtitle,
+                    color: palette.text.secondary,
+                  },
+                  '.MuiListItemText-secondary': {
+                    ...typography.titleSubtitle.title,
+                    color: palette.text.disabled,
+                  },
+                }}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
   )
+}
+
+function TopFade({ children }: { children: ReactNode }) {
+  const { constants, palette, typography } = useTheme()
+
+  return (
+    <Box
+      // sx={{
+      //   position: 'relative',
+      //   ':after': {
+      //     content: '""',
+      //     position: 'absolute',
+      //     bottom: 0,
+      //     left: 0,
+      //     pointerEvents: 'none',
+      //     background: `linear-gradient(to bottom, rgba(255,255,255, 0), ${palette.background.default} 90%)`,
+      //     width: '100%',
+      //     height: '4em',
+      //   },
+      // }}
+      sx={{
+        ...fadeStyles,
+        bottom: 0,
+        background: `linear-gradient(to bottom, rgba(255,255,255, 0), ${palette.background.default} 90%)`,
+      }}
+    >
+      {children}
+    </Box>
+  )
+}
+
+const fadeStyles = {
+  position: 'relative',
+  ':after': {
+    content: '""',
+    position: 'absolute',
+    left: 0,
+    pointerEvents: 'none',
+    width: '100%',
+    height: '4em',
+  },
 }
