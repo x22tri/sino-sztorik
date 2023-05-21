eb5cb6bd07e912494eee525e00c469d687dc462a
x22tri <x22tri@gmail.com>
2023-05-21 22:29:16 +0200

    Finalize OtherUses

--- src/new-components/learn/phrases/Phrases.tsx
+++ src/new-components/learn/phrases/Phrases.tsx
@@ -28,36 +28,50 @@ export function PhrasesAndOtherUses({
     >
       <When condition={phrases?.length}>
         <Subheading title={LEARN_SUBHEADING_PHRASES} styles={{ gridColumn: '1 / -1', mx: -2, my: 2 }} />
-
-        {phrases?.map(({ characters, phraseHungarian }, index) => (
-          <Fragment key={index}>
-            <Box display='flex'>
-              {characters.map(({ charChinese, keyword, pinyin, primitiveMeaning }, charIndex) => (
-                <ChineseCharLink key={charIndex} {...{ charChinese, currentChar, keyword, pinyin, primitiveMeaning }} />
-              ))}
-            </Box>
-            {phraseHungarian}
-          </Fragment>
-        ))}
+        <Phrases phrases={phrases!} {...{ currentChar }} />
       </When>
 
       <When condition={otherUses?.length}>
         <Subheading title={LEARN_SUBHEADING_OTHER_USES} styles={{ gridColumn: '1 / -1', mx: -2, my: 2 }} />
-
-        {otherUses?.map(({ pinyin, meanings }) => (
-          <Fragment key={pinyin}>
-            <Box component='span' fontWeight={500} gridColumn='1 / 2' typography='presentation.pinyin'>
-              {pinyin}
-            </Box>
-
-            {meanings.map(meaning => (
-              <Box component='span' key={meaning} gridColumn='2 / 3' marginTop={-2}>
-                {meaning}
-              </Box>
-            ))}
-          </Fragment>
-        ))}
+        <OtherUses otherUses={otherUses!} />
       </When>
     </Box>
+  )
+}
+
+function Phrases({ currentChar, phrases }: { currentChar: string; phrases: Phrase[] }) {
+  return (
+    <>
+      {phrases.map(({ characters, phraseHungarian }, index) => (
+        <Fragment key={index}>
+          <Box display='flex'>
+            {characters.map(({ charChinese, keyword, pinyin, primitiveMeaning }, charIndex) => (
+              <ChineseCharLink key={charIndex} {...{ charChinese, currentChar, keyword, pinyin, primitiveMeaning }} />
+            ))}
+          </Box>
+          {phraseHungarian}
+        </Fragment>
+      ))}
+    </>
+  )
+}
+
+function OtherUses({ otherUses }: { otherUses: OtherUse[] }) {
+  return (
+    <>
+      {otherUses.map(({ pinyin, meanings }) => (
+        <Fragment key={pinyin}>
+          <Box component='span' fontWeight={500} gridColumn='1 / 2' typography='presentation.pinyin'>
+            {pinyin}
+          </Box>
+
+          {meanings.map(meaning => (
+            <Box component='span' key={meaning} gridColumn='2 / 3' marginTop={-0.5}>
+              {meaning}
+            </Box>
+          ))}
+        </Fragment>
+      ))}
+    </>
   )
 }
