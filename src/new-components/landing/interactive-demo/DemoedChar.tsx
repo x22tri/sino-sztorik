1c2a0d8ba44be5eb67ee474d0f16996c09e39319
x22tri <x22tri@gmail.com>
2023-05-24 23:58:22 +0200

    Fix animation

--- src/new-components/landing/interactive-demo/DemoedChar.tsx
+++ src/new-components/landing/interactive-demo/DemoedChar.tsx
@@ -1,6 +1,6 @@
 import { Box, Grow, Typography } from '@mui/material'
 import { DemoContentChar } from './demoContent'
-import { Dispatch, SetStateAction } from 'react'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import { Segment as SegmentType } from '../../shared/interfaces'
 import { StorySegmentResolverDemo } from './StorySegmentResolverDemo'
 
@@ -12,28 +12,32 @@ export function DemoedChar({
   setDemoedCharChinese: Dispatch<SetStateAction<string>>
 }) {
   return (
-    <Grow in={true}>
-      <Box
-        bgcolor='background.paper'
-        borderRadius={1}
-        margin='auto'
-        maxWidth='24ch'
-        padding={1}
-        textAlign='justify'
-        typography='chineseText'
-      >
-        <Typography display='flex' justifyContent='center' variant='chineseText' fontSize={80} width={1}>
-          {char.charChinese}
-        </Typography>
+    <Box
+      bgcolor='background.paper'
+      borderRadius={1}
+      key={char.charChinese}
+      margin='auto'
+      maxWidth='24ch'
+      padding={1}
+      textAlign='justify'
+      typography='chineseText'
+      sx={{
+        animationDuration: ({ constants }) => constants.animationDuration,
+        animationName: 'fade-in',
+        '@keyframes fade-in': { '0%': { opacity: 0 }, '100%': { opacity: 1 } },
+      }}
+    >
+      <Typography display='flex' justifyContent='center' variant='chineseText' fontSize={80} width={1}>
+        {char.charChinese}
+      </Typography>
 
-        <Typography display='flex' justifyContent='center' variant='h4'>
-          {char.keyword}
-        </Typography>
+      <Typography display='flex' justifyContent='center' variant='h4'>
+        {char.keyword}
+      </Typography>
 
-        {char.story.map((paragraph, index) => (
-          <StorySegmentResolverDemo key={index} segments={paragraph as SegmentType[]} {...{ setDemoedCharChinese }} />
-        ))}
-      </Box>
-    </Grow>
+      {char.story.map((paragraph, index) => (
+        <StorySegmentResolverDemo key={index} segments={paragraph as SegmentType[]} {...{ setDemoedCharChinese }} />
+      ))}
+    </Box>
   )
 }
