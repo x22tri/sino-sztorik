7b0019f3875779c65ee802309e1df87fd3eb49c8
x22tri <x22tri@gmail.com>
2023-06-17 13:41:03 +0200

    Add query params

--- src/new-components/admin/admin-content/AdminContent.tsx
+++ src/new-components/admin/admin-content/AdminContent.tsx
@@ -1,106 +1,36 @@
-import { Chip, Stack, Step, StepContent, Stepper, useTheme, useThemeProps } from '@mui/material'
-import { useFetcher, useLoaderData } from 'react-router-dom'
+import { Stack, Step, StepContent, Stepper, useTheme } from '@mui/material'
+import { useFetcher, useLoaderData, useSearchParams } from 'react-router-dom'
 import { Heading } from '../../learn/headings/Heading'
 import { CharacterSection } from './sections/CharacterSection'
-import { ForwardedRef, forwardRef, useState } from 'react'
-import { IconDefinition, faPen } from '@fortawesome/free-solid-svg-icons'
-import ToolbarButton from '../../shared/components/ToolbarButton'
-import { CHAR_ENTRY, CharacterEntry } from '../../shared/MOCK_DATABASE_ENTRIES'
+import { CHAR_ENTRY } from '../../shared/MOCK_DATABASE_ENTRIES'
 import { Else, If, Then } from 'react-if'
 import { LocationInLessonPreview } from './LocationInLessonPreview'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Subheading } from '../../learn/headings/Subheading'
 import { DiffedCharacterEntry } from '../../shared/logic/loadAdminChar'
 import { PreviewCharacterVariant } from './preview-character-variant/PreviewCharacterVariant'
 import { AddCharacterVariant } from './add-character-variant/AddCharacterVariant'
 
-const CHAR_ENTRY_CONVERTED = {
-  charChinese: '早',
-  lessonNumber: 2,
-  phrases: [1, 2], // An array of phrase ID's.
-  similars: [3], // An array of "similar" ID's.
-  variants: [
-    {
-      constituents: ['日', '十'], // Call getUnlockedMeanings before presenting to the user.
-      frequency: 462,
-      index: 11,
-      keyword: 'korai',
-      otherUses: [
-        { pinyin: 'zhèng', meanings: ['test1', 'test2'] },
-        { pinyin: 'zhēng', meanings: ['test3'] },
-      ],
-      pinyin: 'zǎo',
-      story: [
-        [
-          `Egy `,
-          { constituent: 'nap', references: '日' },
-          ` amely egy `,
-          { constituent: 'tűn', references: '十' },
-          `pihen meg – gyakorlatilag ilyen egy napraforgó, annak vékony szárával és ragyogó virágával.`,
-        ],
-        [
-          `Ismeretes, hogy azért hívjuk a napraforgót napraforgónak, mert követi a `,
-          { constituent: 'nap', references: '日' },
-          `mozgását – magyarán, a kertben a `,
-          { constituent: 'nap', references: '日' },
-          ` erre a virágra süt a leg`,
-          { keyword: 'korábban' },
-          `. Nem csoda hát, hogy a karakter jelentése `,
-          { keyword: '„korai”' },
-          `.`,
-        ],
-      ],
-      tier: 1,
-    },
-    {
-      index: 8,
-      primitive: 'napraforgó',
-      story: [
-        [
-          `Egy `,
-          { constituent: 'nap', references: '日' },
-          ` amely egy `,
-          { constituent: 'tűn', references: '十' },
-          `pihen meg – gyakorlatilag ilyen egy `,
-          { primitive: 'napraforgó' },
-          `, annak vékony szárával és ragyogó virágával.`,
-        ],
-        [
-          `Ismeretes, hogy azért hívjuk a `,
-          { primitive: 'napraforgót napraforgónak' },
-          `, mert követi a `,
-          { constituent: 'nap', references: '日' },
-          `mozgását – magyarán, a kertben a `,
-          { constituent: 'nap', references: '日' },
-          ` erre a virágra süt a leg`,
-          { keyword: 'korábban' },
-          `. Nem csoda hát, hogy a karakter jelentése `,
-          { keyword: '„korai”' },
-          `.`,
-        ],
-      ],
-      tier: 2,
-    }, // When there's a "primitive" property on a variant that isn't the first one in the array, a "newPrimitive" flag is added.
-    {
-      // index: 2,
-      // tier: 3,
-    }, // When only these two properties exist on a variant, a "reminder" flag is added.
-  ],
-}
-
 export default function AdminContent() {
   const fetcher = useFetcher()
-  const [activeStep, setActiveStep] = useState(-1)
   const { constants } = useTheme()
   const character = useLoaderData() as DiffedCharacterEntry
+  const [searchParams, setSearchParams] = useSearchParams()
+  const activeTier = Number(searchParams.get('tier'))
 
-  function changeTier(index: number) {
+  function changeTier(tier: number) {
     // Fetch data by merging previous tiers.
     // Idea - have two objects: "previousInfo" and "newInfo".
     // Add simple boxes with Edit button (pencil) for "previousInfo" properties and textboxes for "newInfo" properties.
     // Clicking on Edit turns a simple box into a textbox, removes the property from previousInfo and adds it into newInfo.
     // This character object will then be the basis for rendering below.
-    setActiveStep(activeStep === index ? -1 : index)
+    // let activeTier = Number(searchParams.get('tier'))
+
+    if (tier === activeTier) {
+      searchParams.delete('tier')
+      setSearchParams(searchParams)
+    } else {
+      setSearchParams({ tier: String(tier) })
+    }
   }
 
   function mergePreviousTiers(charEntry: typeof CHAR_ENTRY, tierToStopAt: number) {
@@ -131,19 +61,19 @@ export default function AdminContent() {
     >
       <Heading title='Idővonal' />
 
-      <Stepper nonLinear orientation='vertical' {...{ activeStep }}>
-        {[1, 2, 3, 4].map((step, index) => (
+      <Stepper nonLinear orientation='vertical' activeStep={activeTier - 1 ?? -1}>
+        {[1, 2, 3, 4].map((tier, index) => (
           <Step key={index}>
             <If condition={!isNotPresentInTier(character.variants[index])}>
               <Then>
                 <PreviewCharacterVariant
-                  isActive={step - 1 === activeStep}
+                  isActive={tier === activeTier}
                   variant={character.variants[index]}
-                  onClick={() => changeTier(step - 1)}
+                  onClick={() => changeTier(tier)}
                 />
               </Then>
               <Else>
-                <AddCharacterVariant onClick={() => changeTier(step - 1)} />
+                <AddCharacterVariant onClick={() => changeTier(tier)} />
               </Else>
             </If>
 
@@ -190,7 +120,7 @@ export default function AdminContent() {
 
               <LocationInLessonPreview
                 lessonPreview={[{ charChinese: '世', index: 10 }, { charChinese: '早', index: 11 }, null]}
-                onClick={() => changeTier(step - 1)}
+                onClick={() => {}}
               />
             </StepContent>
           </Step>
