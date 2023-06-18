04c2e5618ef69d0d555f069d2d949c6a06ec90eb
x22tri <x22tri@gmail.com>
2023-06-18 21:09:27 +0200

    Change disabled input background color

--- src/new-components/admin/admin-content/AdminTextField.tsx
+++ src/new-components/admin/admin-content/AdminTextField.tsx
@@ -1,20 +1,11 @@
 import { InputAdornment, TextField, TextFieldProps, lighten, useTheme } from '@mui/material'
-import { Controller, useForm, useFormContext } from 'react-hook-form-mui'
+import { Controller, useFormContext } from 'react-hook-form-mui'
 import { X } from './AdminContent'
 import { useStore } from '../../shared/logic/useStore'
 import { faPen } from '@fortawesome/free-solid-svg-icons'
 import ToolbarButton from '../../shared/components/ToolbarButton'
 import { useEffect, useRef, useState } from 'react'
-import { Unless, When } from 'react-if'
-
-/*
-
-To-Do:
-if new info: add colored BG to text field, add field name to newInfo, add value as defaultValue if present
-if modified info: add colored BG to text field, add field name to modifiedInfo, add value as defaultValue if present
-if previous info: disable text field and make BG white, add pen endAdornment that lets you edit it
-
-*/
+import { When } from 'react-if'
 
 export function AdminTextField({ label, name, ...restProps }: TextFieldProps & { name: keyof X }) {
   const inputRef = useRef<HTMLInputElement>(null)
@@ -76,7 +67,12 @@ export function AdminTextField({ label, name, ...restProps }: TextFieldProps & {
           variant='filled'
           {...restProps}
           {...{ label, name, value }}
-          sx={{ flexShrink: 1, '.MuiFilledInput-root': { bgcolor, ':hover': { bgcolor: hoverBgColor } }, ...restProps.sx }}
+          sx={{
+            flexShrink: 1,
+            '.MuiFilledInput-root': { bgcolor, ':hover': { bgcolor: hoverBgColor } },
+            '.Mui-disabled': { bgcolor: 'inherit', color: 'inherit' },
+            ...restProps.sx,
+          }}
         />
       )}
     />
