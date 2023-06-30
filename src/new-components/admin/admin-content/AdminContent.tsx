de16890dfea06922a86cd5dd746cf2e351012a51
x22tri <x22tri@gmail.com>
2023-06-30 21:44:01 +0200

    Create AdminStepLabel

--- src/new-components/admin/admin-content/AdminContent.tsx
+++ src/new-components/admin/admin-content/AdminContent.tsx
@@ -1,48 +1,104 @@
-import { Box, Divider, Stack, Step, StepButton, StepContent, Stepper, Typography, useTheme } from '@mui/material'
+import { Box, Step, StepLabel, Stepper, useTheme } from '@mui/material'
 import { useLoaderData } from 'react-router-dom'
-import { Heading } from '../../learn/headings/Heading'
-import { SortedCharacterEntry } from '../../shared/logic/loadAdminChar'
+import { SortedCharacterEntry, SortedOccurrences } from '../../shared/logic/loadAdminChar'
 import { CharacterEntryVariant } from '../../shared/MOCK_DATABASE_ENTRIES'
 import { Timeline } from '../timeline/Timeline'
-import { useEffect, useState } from 'react'
-import { When } from 'react-if'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import { isFullOccurrence, isWithheldKeywordOccurrence, isWithheldPrimitiveOccurrence } from '../utils/occurrence-utils'
-import { TimelineErrors } from './TimelineErrors'
+import { AdminStepLabel } from './AdminStepLabel'
 
 export function mergePreviousTiers(variants: CharacterEntryVariant[], tierToStopAt: number) {
   return variants.slice(0, tierToStopAt).reduce((previousInfo, newInfo) => Object.assign(previousInfo, newInfo), {})
 }
 
+export enum TimelineError {
+  MissingStory = 'MissingStory',
+  KeywordNotIntroduced = 'KeywordNotIntroduced',
+  PrimitiveNotIntroduced = 'PrimitiveNotIntroduced',
+  CourseLocationNotSet = 'CourseLocationNotSet',
+}
+
 export default function AdminContent() {
-  const { constants } = useTheme()
+  const { constants, palette, spacing } = useTheme()
   const { character } = useLoaderData() as { character: SortedCharacterEntry }
   const [occurrences, setOccurrences] = useState(character.occurrences)
-  const [activeStep, setActiveStep] = useState(0)
+  const [activeStep, setActiveStep] = useState(1)
+  const [timelineErrors, setTimelineErrors] = useState<TimelineError[]>([])
+
+  useTimelineErrors(character, occurrences, setTimelineErrors)
 
   return (
-    <Stack
-      boxSizing='border-box'
+    <Box
       bgcolor='background.paper'
       component='main'
-      display='flex'
-      flexDirection='column'
       minHeight={`calc(100vh - ${constants.bottomToolbarHeight})`}
       paddingX={2}
+      width={1}
     >
-      <Stepper orientation='vertical' {...{ activeStep }}>
+      <Stepper {...{ activeStep }}>
         <Step>
-          <StepButton sx={{ '.MuiStepLabel-root': { width: 1 } }}>
-            <Stack alignItems='center' direction='row' gap={1} justifyContent='space-between'>
-              <Typography variant='h6'>Idővonal</Typography>
-              <TimelineErrors {...{ character, occurrences }} />
-            </Stack>
-          </StepButton>
+          <StepLabel>Karakter</StepLabel>
+        </Step>
 
-          <StepContent>
-            <Timeline {...{ character, occurrences, setOccurrences }} />
-          </StepContent>
+        <Step>
+          <AdminStepLabel errorsArray={timelineErrors} errorMessages={timelineErrorStrings} title='Idővonal' />
+        </Step>
+
+        <Step>
+          <StepLabel>Ellenőrzés</StepLabel>
         </Step>
       </Stepper>
-    </Stack>
+
+      <Timeline {...{ character, occurrences, setOccurrences }} />
+    </Box>
+
+    // <StepContent>
+    //       <Timeline {...{ character, occurrences, setOccurrences }} />
+    //     </StepContent>
+
+    // {activeStep === 0 ? 'Űrlap' : false}
+
+    // {activeStep === 1 ? <Timeline {...{ character, occurrences, setOccurrences }} /> : false}
   )
+}
+
+function useTimelineErrors(
+  character: SortedCharacterEntry,
+  occurrences: SortedOccurrences,
+  setTimelineErrors: Dispatch<SetStateAction<TimelineError[]>>
+) {
+  useEffect(() => {
+    const errors: TimelineError[] = []
+
+    if (occurrences.some(occurrence => 'story' in occurrence && occurrence.story.length === 0)) {
+      errors.push(TimelineError.MissingStory)
+    }
+
+    if (
+      'keyword' in character &&
+      !occurrences.some(occurrence => isFullOccurrence(occurrence) || isWithheldPrimitiveOccurrence(occurrence))
+    ) {
+      errors.push(TimelineError.KeywordNotIntroduced)
+    }
+
+    if (
+      'primitive' in character &&
+      !occurrences.some(occurrence => isFullOccurrence(occurrence) || isWithheldKeywordOccurrence(occurrence))
+    ) {
+      errors.push(TimelineError.PrimitiveNotIntroduced)
+    }
+
+    if (occurrences.some(occurrence => 'index' in occurrence && occurrence.index === 0)) {
+      errors.push(TimelineError.CourseLocationNotSet)
+    }
+
+    setTimelineErrors(errors)
+  }, [character, occurrences])
+}
+
+const timelineErrorStrings: Record<TimelineError, string> = {
+  MissingStory: 'Nincs történet legalább egy előfordulásnál',
+  KeywordNotIntroduced: 'A kulcsszó nincs bevezetve',
+  PrimitiveNotIntroduced: 'Az alapelem nincs bevezetve',
+  CourseLocationNotSet: 'Legalább egy előfordulás nincs elhelyezve a leckében',
 }
--- src/new-components/shared/utility-components.tsx
+++ src/new-components/shared/utility-components.tsx
@@ -1,20 +1,16 @@
 import { Box, useTheme } from '@mui/material'
-import { ReactNode, FC } from 'react'
+import { ReactNode, FC, ReactElement } from 'react'
 
 const { isArray } = Array
 
 // Conditionally renders a wrapper based on the truthiness of the 'if' prop.
 interface WrapProps {
   if: any
-  with: (children: ReactNode) => JSX.Element
+  with: (children: ReactElement) => JSX.Element
   children: JSX.Element | JSX.Element[]
 }
 
-export const Wrap: FC<WrapProps> = ({
-  if: condition,
-  with: wrapper,
-  children,
-}) => {
+export const Wrap: FC<WrapProps> = ({ if: condition, with: wrapper, children }) => {
   const renderedChildren = isArray(children) ? <>{children}</> : children
 
   return !!condition ? wrapper(renderedChildren) : renderedChildren
